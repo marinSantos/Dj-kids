c9e165c38623a7031614a656233258eba35b6dfa
Lucas Marin <96667789+marinSantos@users.noreply.github.com>
2023-02-24 19:47:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* alert('Atualização: \n\n-Agora quando o modo alternar estiver ativo, a invés das músicas pausarem, irão parar completamente.\n\n-Modo alternar pré-ativo como padrão.\n\n-Recurso sem ultilidade retirado da sessão de músicas "caso ache um equívoco, entrar em contato."\n\n-Nova ultilidade adicionada na sessão de músicas.') */
+
+
 //Deixa a cor de fundo do header transparente
 window.onscroll = function () { transparent() }
 
@@ -41,76 +44,8 @@ const a3 = document.getElementById('a3')
 const a4 = document.getElementById('a4')
 const a5 = document.getElementById('a5')
 const a6 = document.getElementById('a6')
-const bola = document.getElementById('bola')
-const cheeck = document.getElementById('cheeck')
-
-pause.addEventListener('click', ball)
-
-function ball() {
-  if (bola.className == 'bolaDesactive') {
-    cheeck.className = 'checkActive'
-    bola.className = 'bolaActive'
-    pause.addEventListener('click', addEv())
-  } else {
-     cheeck.className = 'checkDesactive'
-    bola.className = 'bolaDesactive'
-    pause.addEventListener('click', reloadDj())
-  }
-}
-
-function reloadDj() {
-  a1.removeEventListener('play', a1ev)
-  a2.removeEventListener('play', a2ev)
-  a3.removeEventListener('play', a3ev)
-  a4.removeEventListener('play', a4ev)
-  a5.removeEventListener('play', a5ev)
-  a6.removeEventListener('play', a6ev)
-
-}
 
 
-function a1ev() {
-  a2.pause()
-  a3.pause()
-  a4.pause()
-  a5.pause()
-  a6.pause()
-}
-function a2ev() {
-  a1.pause()
-  a3.pause()
-  a4.pause()
-  a5.pause()
-  a6.pause()
-}
-function a3ev() {
-  a2.pause()
-  a1.pause()
-  a4.pause()
-  a5.pause()
-  a6.pause()
-}
-function a4ev() {
-  a2.pause()
-  a3.pause()
-  a1.pause()
-  a5.pause()
-  a6.pause()
-}
-function a5ev() {
-  a2.pause()
-  a3.pause()
-  a4.pause()
-  a1.pause()
-  a6.pause()
-}
-function a6ev() {
-  a2.pause()
-  a3.pause()
-  a4.pause()
-  a5.pause()
-  a1.pause()
-}
 
 function addEv() {
 
@@ -121,30 +56,153 @@ function addEv() {
   a5.addEventListener('play', a5ev)
   a6.addEventListener('play', a6ev)
 }
+addEv()
+
+
+
+function a1ev() {
+  a2.pause()
+  a3.pause()
+  a4.pause()
+  a5.pause()
+  a6.pause()
+
+  a2.myAudio.currentTime = 0
+  a3.myAudio.currentTime = 0
+  a4.myAudio.currentTime = 0
+  a5.myAudio.currentTime = 0
+  a6.myAudio.currentTime = 0
+}
+function a2ev() {
+  a1.pause()
+  a3.pause()
+  a4.pause()
+  a5.pause()
+  a6.pause()
+
+  a1.currentTime = 0
+  a3.currentTime = 0
+  a4.currentTime = 0
+  a5.currentTime = 0
+  a6.currentTime = 0
+
+}
+function a3ev() {
+  a2.pause()
+  a1.pause()
+  a4.pause()
+  a5.pause()
+  a6.pause()
+
+  a2.currentTime = 0
+  a1.currentTime = 0
+  a4.currentTime = 0
+  a5.currentTime = 0
+  a6.currentTime = 0
+}
+function a4ev() {
+  a2.pause()
+  a3.pause()
+  a1.pause()
+  a5.pause()
+  a6.pause()
+
+  a2.currentTime = 0
+  a3.currentTime = 0
+  a1.currentTime = 0
+  a5.currentTime = 0
+  a6.currentTime = 0
+}
+function a5ev() {
+  a2.pause()
+  a3.pause()
+  a4.pause()
+  a1.pause()
+  a6.pause()
+
+  a2.currentTime = 0
+  a3.currentTime = 0
+  a4.currentTime = 0
+  a1.currentTime = 0
+  a6.currentTime = 0
+}
+function a6ev() {
+  a2.pause()
+  a3.pause()
+  a4.pause()
+  a5.pause()
+  a1.pause()
+
+  a2.currentTime = 0
+  a3.currentTime = 0
+  a4.currentTime = 0
+  a5.currentTime = 0
+  a1.currentTime = 0
+}
+
+
 
 
 const link = document.getElementById('linkYoutube')
+const link2 = document.getElementById('linkYoutube2')
+const link3 = document.getElementById('pesquisa')
 const enviar = document.getElementById('enviar')
+const enviar2 = document.getElementById('enviar2')
+const enviar3 = document.getElementById('enviar3')
 const youtube = document.getElementById('youtube')
 const original = document.getElementById('original')
 
-enviar.addEventListener('click',function(ev){
+enviar.addEventListener('click', function (ev) {
   ev.preventDefault()
 
-  
+
   let pesquisa = link.value
-  youtube.src = "https://www.youtube.com/embed/"+pesquisa
+  youtube.src = "https://www.youtube.com/embed/" + pesquisa
   link.value = ''
 })
+
+
+enviar2.addEventListener('click', function (ev) {
+  ev.preventDefault()
+
+  let pesquisa = link2.value
+  let slice = pesquisa.match(/.+\?v=/g)
+  let result = pesquisa.replace(slice, "https://www.youtube.com/embed/")
+  youtube.src = result
+  link2.value = ''
+})
+let index = 0
+enviar3.addEventListener('click', function (ev) {
+  ev.preventDefault()
+  alert('Resposta enviada!')
+  index++
+
+  localStorage.setItem('resposta' + index, link3.value)
+  link3.value = ''
+}
+)
 
 
 //Scroll automático
 
 const logo = document.getElementById('logo')
 
-logo.addEventListener('click',function(){
-  window.scroll(0,0)
+logo.addEventListener('click', function () {
+  window.scroll(0, 0)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
